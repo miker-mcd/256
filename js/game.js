3273246a805694af672d6710ff4d2dec1fe5ba5f
miker-mcd <michaelrmcd@gmail.com>
2018-01-29 12:51:47 -0800

    Rename combine method to collapseRight and refactor

--- js/game.js
+++ js/game.js
@@ -1,44 +1,48 @@
-// Generate a string of 16 zeros
-// => '0000000000000000';
-
-// Replace a 2 or 4 in two slots in the string
-// => '0000200020000000';
-
-// Use toString() to split string into four lines of four characters
-// => '0000
-    // 2020
-    // 0000
-    // 0000'
-
-// const combine = function (line) {
+// const collapseRight = function (line) {
   // Input: array of four numbers
   // if line contains all zeros, return array
   // if nums in line equal to 2, replace last slot with 2
   // Output: a new array of four numbers with identical nums added
 // }
 
-const combine = function (line) {
-  var sum = line.reduce(function (acc, val) {
-    return acc + val;
-  }, 0);
-  if (sum == 2) {
-    line = [0, 0, 0, 2];
+function collapseRight (row) {
+  var filtered = row.filter(function (num) {
+    return num > 0;
+  })
+
+  if (filtered.length == 1) {
+    row = [0,0,0];
+    row.push(filtered[0])
   }
-  return line;
+  // else if (row.length > 3) {
+  //   row = 'pizza';
+  // }
+  return row;
 }
 
-const Game = function (string) {
-  var board = '0000000000000000';
+const Board = function (str) {
+  // Generate a string of 16 zeros
+  // => '0000000000000000';
 
-  return {
-    "board": string || board,
-    "toString": function () {},
-    "move": function (direction) {
-      // Input: array of nums (0, 2, 4)
-    }
-  }
+  // Replace a 2 or 4 in two slots in the string
+  // => '0000200020000000';
 }
 
-// var game = new Game('0000202000000000');
-var game = new Game();
+const Game = function (str) {
+  this.board = str.split().map(function (char) {
+    return parseInt(char);
+  }); // || new Board();
+
+  this.toString = function () {}
+    // Use toString() to split string into four lines of four characters
+    // => '0000
+        // 2020
+        // 0000
+        // 0000'
+
+  this.move = function (direction) {}
+}
+
+var game = new Game('0000202000000000');
+// var game = new Game();
 console.log(game)
