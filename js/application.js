22b0fd9b2cb8df80feaf561d0b1de8f4111041e0
miker-mcd <michaelrmcd@gmail.com>
2018-02-08 19:36:34 -0800

    Replace array for string to fix bug in #toString

--- js/application.js
+++ js/application.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   var game = new Game('0000202022402882');
   $(".game").append(game.toString());
   Mousetrap.bind('right', function () {
-    game = new Game(game.move('right'));
+    game.move('right');
+    // game = new Game(game.move('right'));
     $('.game').html(game.toString());
   });
 });
--- js/game.js
+++ js/game.js
@@ -1,16 +1,16 @@
 function spawnBlock(board) {
-    board = board.split('').map(function (char) {
-      return parseInt(char, 10);
-    }) // => [0,0,0,0,0,0,0,4,0,0,4,4,0,2,8,2]
+    // board = board.split('').map(function (char) {
+    //   return parseInt(char, 10);
+    // }) // => [0,0,0,0,0,0,0,4,0,0,4,4,0,2,8,2]
     var blocks = [2, 4];
     function insertBlock(board) {
-      // debugger
-      let randomIndex = Math.floor(Math.random() * Math.floor(board.length));
+      let randomRow = Math.floor(Math.random() * Math.floor(board.length));
+      let randomIndex = Math.floor(Math.random() * Math.floor(board[randomRow].length));
       let newBlock = blocks[(Math.floor(Math.random() * Math.floor(2)))];
 
-      if (board[randomIndex] < 2) {
-        board.splice(randomIndex, 1, newBlock)
-        board = board.join('');
+      if (board[randomRow][randomIndex] < 1) {
+        board[randomRow].splice(randomIndex, 1, newBlock)
+        // board = board.join('');
         return board;
       }
       else {
@@ -23,87 +23,6 @@ function spawnBlock(board) {
   return newBoard;
 }
 
-function collapseRight (board) {
-  var newBoard = [];
-
-  var splitBoard = [];
-  for(let i = 0; i < board.length; i += 4) {
-     splitBoard.push(board.substr(i, 4))
-  }
-
-  splitBoard.forEach(function (row) {
-    debugger
-    row = row.split('').map(function (char) {
-      return parseInt(char, 10);
-    }) // => [2,0,2,0]
-
-    var filtered = row.filter(function (num) {
-      return num > 0;
-    }) // => [2,2]
-
-    var rowSum = filtered.reduce(function (acc, num) {
-      return acc + num;
-    }, 0) // => 4
-
-    if (filtered.length > 3) {
-      var check = filtered[0];
-      for (let i = 1;i < filtered.length;i++) {
-        if (check == filtered[i]) {
-          var sumOfPair = check + filtered[i];
-          filtered.splice(i, 1, sumOfPair);
-          filtered.splice(filtered.indexOf(check), 1)
-        }
-        check = filtered[i];
-      }
-      var zerosToAdd = row.length - filtered.length;
-      for (let i = 0;i < zerosToAdd;i++) {
-        filtered.unshift(0);
-      }
-      row = filtered.join('');
-    }
-
-    else if (filtered.length > 2) {
-      var check = filtered[0];
-      for (let i = 1;i < filtered.length;i++) {
-        if (check == filtered[i]) {
-          var sumOfPair = check + filtered[i];
-          filtered.splice(i, 1, sumOfPair);
-          filtered.splice(filtered.indexOf(check), 1);
-        } else {
-          check = filtered[i];
-        }
-      }
-      var zerosToAdd = row.length - filtered.length;
-      for (let i = 0;i < zerosToAdd;i++) {
-        filtered.unshift(0);
-      }
-      row = filtered.join('');
-    }
-
-    else if (filtered.length > 1) {
-      if (filtered[0] == filtered[1]) {
-        filtered.splice(1, 1, rowSum);
-        filtered.shift();
-        filtered.unshift(0, 0, 0);
-        row = filtered.join('');
-      } else {
-        filtered.unshift(0, 0);
-        row = filtered.join('');
-      }
-    }
-
-    // else if (filtered.length >= 0) {
-    else {
-      row = [0,0,0];
-      row.push(rowSum);
-      row = row.join('');
-    }
-    newBoard.push(row);
-  })
-  newBoard = newBoard.join('');
-  return spawnBlock(newBoard);
-}
-
 // TODO
 const Board = function (str) {
   // Generate a string of 16 zeros
@@ -114,19 +33,117 @@ const Board = function (str) {
 }
 
 const Game = function (str) {
-  this.board = str // || new Board(); => '2020'
+  // this.board = str // || new Board(); => '2020'
+  this.board = str.split('').map(function (char) {
+    return parseInt(char, 10);
+  }); // => [0,0,0,0,2,0,2,0,2,2,4,0,0,2,8,2]
+  var ret = [];
+  for(let i = 0; i < this.board.length; i += 4) {
+     ret.push(this.board.slice(i, i + 4))
+  }
+  this.board = ret;
 
   this.toString = function () {
-    var ret = [];
-    for(let i = 0; i < str.length; i += 4) {
-       ret.push(str.substr(i, 4))
-    }
-    return ret.join('<br>');
+    debugger
+    // var ret = [];
+    // for(let i = 0; i < this.board.length; i += 4) {
+    //    ret.push(this.board.slice(i, i + 4))
+    // }
+    // this.board = ret;
+    return this.board.join('<br>');
+  }
+
+  function collapseRight (board) {
+    var newBoard = [];
+
+    var splitBoard = [];
+    // for(let i = 0; i < board.length; i += 4) {
+    //    splitBoard.push(board.substr(i, 4))
+    // }
+
+    // splitBoard.forEach(function (row) {
+    board.forEach(function (row) {
+      // row = row.split('').map(function (char) {
+      //   return parseInt(char, 10);
+      // }) // => [2,0,2,0]
+
+      var filtered = row.filter(function (num) {
+        return num > 0;
+      }) // => [2,2]
+
+      var rowSum = filtered.reduce(function (acc, num) {
+        return acc + num;
+      }, 0) // => 4
+
+      if (filtered.length > 3) {
+        var check = filtered[0];
+        for (let i = 1;i < filtered.length;i++) {
+          if (check == filtered[i]) {
+            var sumOfPair = check + filtered[i];
+            filtered.splice(i, 1, sumOfPair);
+            filtered.splice(filtered.indexOf(check), 1)
+          }
+          check = filtered[i];
+        }
+        var zerosToAdd = row.length - filtered.length;
+        for (let i = 0;i < zerosToAdd;i++) {
+          filtered.unshift(0);
+        }
+        row = filtered;
+        // row = filtered.join('');
+      }
+
+      else if (filtered.length > 2) {
+        var check = filtered[0];
+        for (let i = 1;i < filtered.length;i++) {
+          if (check == filtered[i]) {
+            var sumOfPair = check + filtered[i];
+            filtered.splice(i, 1, sumOfPair);
+            filtered.splice(filtered.indexOf(check), 1);
+          } else {
+            check = filtered[i];
+          }
+        }
+        var zerosToAdd = row.length - filtered.length;
+        for (let i = 0;i < zerosToAdd;i++) {
+          filtered.unshift(0);
+        }
+        // row = filtered.join('');
+        row = filtered;
+      }
+
+      else if (filtered.length > 1) {
+        if (filtered[0] == filtered[1]) {
+          filtered.splice(1, 1, rowSum);
+          filtered.shift();
+          filtered.unshift(0, 0, 0);
+          // row = filtered.join('');
+          row = filtered;
+        } else {
+          filtered.unshift(0, 0);
+          // row = filtered.join('');
+          row = filtered;
+        }
+      }
+
+      // else if (filtered.length >= 0) {
+      else {
+        row = [0,0,0];
+        row.push(rowSum);
+        // row = row.join('');
+      }
+      newBoard.push(row);
+    })
+    // newBoard = newBoard.join('');
+    return spawnBlock(newBoard);
   }
 
   this.move = function (direction) {
     if (direction == 'right') {
-      return collapseRight(this.board);
+      this.board = collapseRight(this.board);
+      return this.board;
+      // return collapseRight(this.board);
     }
   }
+
 }
