925727b9cdb89a6c289decad08e68de15dbd42e0
miker-mcd <michaelrmcd@gmail.com>
2018-03-01 10:01:49 -0800

    Update index to target span with tile class and implement localStorage for savedGame

--- js/application.js
+++ js/application.js
@@ -1,26 +1,56 @@
 $(document).ready(function() {
   var game = new Game();
+  var savedGame = localStorage.getItem('game');
+  var savedScore = localStorage.getItem('score');
+  if (savedGame != 'undefined' || savedGame != 'null') {
+    var el = $('<div></div>');
+    el.html(savedGame);
+    var tiles = $.map(el, function (tile) {
+      return $(tile).text();
+    });
+    game = new Game(tiles.join(''));
+  }
+  if (savedScore != 'undefined' || savedGame != 'null') {
+    $('.score').html(savedScore);
+  } else {
+    $('.score').html(game.updateScore());
+  }
   $(".game").append(game.toString());
-  $('.score').html(game.updateScore());
   Mousetrap.bind('right', function () {
     game.move('right');
     $('.game').html(game.toString());
     $('.score').html(game.updateScore());
+    if (localStorage) {
+      localStorage.setItem('game', $('.game').html());
+      localStorage.setItem('score', $('.score').html());
+    }
   });
   Mousetrap.bind('down', function () {
     game.move('down');
     $('.game').html(game.toString());
     $('.score').html(game.updateScore());
+    if (localStorage) {
+      localStorage.setItem('game', $('.game').html());
+      localStorage.setItem('score', $('.score').html());
+    }
   })
   Mousetrap.bind('left', function () {
     game.move('left');
     $('.game').html(game.toString());
     $('.score').html(game.updateScore());
+    if (localStorage) {
+      localStorage.setItem('game', $('.game').html());
+      localStorage.setItem('score', $('.score').html());
+    }
   })
   Mousetrap.bind('up', function () {
     game.move('up');
     $('.game').html(game.toString());
     $('.score').html(game.updateScore());
+    if (localStorage) {
+      localStorage.setItem('game', $('.game').html());
+      localStorage.setItem('score', $('.score').html());
+    }
   })
   $(document).on('click', '.button', function() {
     game = new Game();
